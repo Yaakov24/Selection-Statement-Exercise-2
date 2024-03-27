d3fe9b8ff9deaddbb099230c23b184c7bc052a72
YaakovG <ygoldenbergyo@gmail.com>
2024-03-27 15:11:43 -0400

    I put a ToLower machiene in it

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string answer = Console.ReadLine();
+
+            switch (answer.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is intellectually stimulating!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is so cool and fun!");
+                    break;
+                case "social studies":
+                    Console.WriteLine("That is a very interesting subject!");
+                    break;
+                case "history":
+                    Console.WriteLine("Everyone should know History");
+                    break;
+                case "english":
+               
+                    Console.WriteLine("That is a neccassary thing to know for all areas");
+                    break;
+                default:
+                    Console.WriteLine("I never heard of it but cool!");
+                    break;
+            }
+                
         }
     }
-}
+};
